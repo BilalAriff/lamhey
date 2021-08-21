d6384cdad2237245277feb8b91a523b9d58bb0a4
Bilal Arif <bilaljamoot1995@gmail.com>
2021-08-21 16:27:55 +0500

    Consultant-Profile Page Complete

--- partials/Head.php
+++ partials/Head.php
@@ -3,6 +3,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="css/font-awesome-4.7/css/font-awesome.css" >
+    <!-- Lightbox gallery CSS -->
+    <link rel="stylesheet" href="css/lightboxgallery.css" >
     <!-- Custom Styles -->
     <link rel="stylesheet" href="css/style.css">
     <title>Home - Lamhey</title>
--- partials/ScriptTags.php
+++ partials/ScriptTags.php
@@ -2,3 +2,4 @@
 <script src="js/popper.js"></script>
 <script src="js/bootstrap.js"></script>
 <script src="js/script.js"></script>
+<script src="js/lightboxgallery.js" >
