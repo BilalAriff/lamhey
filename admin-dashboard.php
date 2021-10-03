029c55d5318e50c9bb9b5745a4aa298faad41462
Bilal Arif <bilaljamoot1995@gmail.com>
2021-10-03 22:57:20 +0500

    complaint managment functionality implemented

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -4,18 +4,20 @@
 
 <!DOCTYPE html>
 <html lang="en">
-    <?php include_once "./partials/Head.php"; ?>
+<?php include_once "./partials/Head.php"; ?>
+
 <body>
     <?php include_once "./partials/Navigation.php" ?>
 
     <!-- Header -->
-    
+
     <div class="jumbotron bg-dark mt-3">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <h3 class="text-white">Admin Dashboard</h3>
-                    <h1 class="text-white">Welcome, <span style="font-weight: bold;"><?php echo $_SESSION['username']?></span></h1>
+                    <h1 class="text-white">Welcome, <span
+                            style="font-weight: bold;"><?php echo $_SESSION['username']?></span></h1>
                 </div>
             </div>
         </div>
@@ -23,25 +25,150 @@
 
     <!-- User Bookings -->
 
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-center my-5">
-                <h3>Monthly Reports</h3>
+    <!-- ============== CONSULTANT MAIN FUNCTIONALITIES ================== -->
+
+    <section class="consultant-options">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center align-items-center">
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#bookings" role="tab"
+                                aria-controls="home" aria-selected="true">Bookings</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#complaints" role="tab"
+                                aria-controls="profile" aria-selected="false">Complaints</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#blockedUsers" role="tab"
+                                aria-controls="contact" aria-selected="false">Blocked Users</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#blockedConsultants" role="tab"
+                                aria-controls="contact" aria-selected="false">Blocked Consultants</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#paymentMethods" role="tab"
+                                aria-controls="contact" aria-selected="false">Payment Methods</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#categories" role="tab"
+                                aria-controls="contact" aria-selected="false">Categories</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="bookings" role="tabpanel" aria-labelledby="home-tab">
+                            <h1 class="text-center my-5 text-uppercase theme-heading">Bookings</h1>
+                            <div class="admin-booking-list-table">
+
+                                <table class="table">
+                                    <thead class="thead-light">
+                                        <tr>
+                                            <th scope="col">ID</th>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">User</th>
+                                            <th scope="col">Event Date</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Booking Date</th>
+                                            <th scope="col">Details</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php echo bookingList($bookingData)?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+                        <div class="tab-pane fade" id="complaints" role="tabpanel" aria-labelledby="profile-tab">
+
+                            <h1 class="text-center theme-heading text-uppercase my-5">Complaints</h1>
+                            <div class="admin-complaint-list-table">
+
+                                <table class="table">
+                                    <thead class="thead-light">
+                                        <tr>
+                                            <th scope="col">ID</th>
+                                            <th scope="col">Complainee</th>
+                                            <th scope="col">Description</th>
+                                            <th scope="col">Complaint Against</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Feedback</th>
+                                            <th scope="col">Actiion</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php echo complaintList($complaintData)?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+                        <div class="tab-pane fade" id="blockedUsers" role="tabpanel" aria-labelledby="contact-tab">
+                            <h1>Blocked Users</h1>
+                        </div>
+
+                        <div class="tab-pane fade" id="blockedConsultants" role="tabpanel"
+                            aria-labelledby="contact-tab">
+                            <h1>Blocked Consultants</h1>
+                        </div>
+
+                        <div class="tab-pane fade" id="paymentMethods" role="tabpanel" aria-labelledby="contact-tab">
+                            <h1>Payment Methods</h1>
+                        </div>
+
+                        <div class="tab-pane fade" id="categories" role="tabpanel" aria-labelledby="contact-tab">
+                            <h1>Categories</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+    <!-- ============== CONSULTANT MAIN FUNCTIONALITIES ENDS ================== -->
+
+    <!-- ================= MODALS ==================== -->
+
+    <!-- Complaint Action Modal
+    <div class="modal fade" id="complaintAction" data-backdrop="static" data-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Understood</button>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12 text-center my-5">
-                <h3>User Profiles</h3>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 text-center my-5">
-                <h3>Consultant Profile</h3>
-            </div>
-        </div>
-    </div>
+    </div> -->
+    <!-- ============== MODALS ENDS ================== -->
 
     <?php include_once "./partials/Footer.php" ?>
     <?php include_once "./partials/ScriptTags.php" ?>
+    <script>
+       let getID = (id) => {
+           console.log(id);
+       }
+    </script>
 </body>
+
 </html>
--- process/admin_dashboard_process.php
+++ process/admin_dashboard_process.php
@@ -5,6 +5,123 @@ include_once "include_files.php";
 $h = new Helper();
 $admin = new Admin("admin");
 
-$bookingList = $admin->getBookingList();
+$bookingData = $admin->getBookingList();
+$complaintData = $admin->getComplaintList();
 
-var_dump($bookingList);
+function bookingTd($booking) {
+
+    $id = $booking['booking_id'];
+    $userId = $booking['booking_user'];
+    $event = $booking['booking_event_name'];
+    $description = $booking['booking_description'];
+    $consultant = $booking['booking_consultant_username'];
+    $date = $booking['booking_date'];
+    $status = $booking['booking_status'];
+    $bookingUsername = $booking['booking_user_username'];
+    $bookingDate = $booking['booking_createdAt'];
+
+    $td = 
+
+        <<<HTML
+         <tr>
+            <td>$id</td>
+            <td>$event</td>
+            <td><a href="user-dashboard.php?id=$userId">$bookingUsername</a></td>
+            <td>$date</td>
+            <td><label class="rounded-pill px-3 py-1 text-uppercase $status" for="">$status</label></td>
+            <td>$bookingDate</td>
+            <td><a href="booking-detail.php?id=$id">View</a></td>
+         <tr>
+        HTML;
+    echo $td;
+}
+
+function complaintTd($complaint) {
+
+    $id = $complaint['id'];
+    $userId = $complaint['user_id'];
+    $username = $complaint['username'];
+    $consultantId = $complaint['consultant_id'];
+    $consultantName = $complaint['consultant_name'];
+    $description = $complaint['description'];
+    $feedback = $complaint['feedback'];
+    $status = $complaint['status'];
+
+    $td = 
+        <<<HTML
+            <tr>
+                <td>$id</td>
+                <td><a href="user-dashboard.php?id=$userId">$username</a></td>
+                <td>$description</td>
+                <td><a href="consultant-profile.php?id=$consultantId">$consultantName</a></td>
+                <td><label class="rounded-pill px-3 py-1 text-uppercase $status" for="">$status</label></td>
+                <td><p>$feedback</p></td>
+                <td>
+                    <a href="" onclick="getID(this.id)" id="$id" data-toggle="modal" data-target="#complaintAction$id">Take Action</a>
+                    <div class="modal fade" id="complaintAction$id" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="staticBackdropLabel">Complaint ID No. $id</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <p class=""><strong>Complaint By:</strong> <a href="user-dashboard.php?id=$userId">$username</a></p>
+                                    <p class=""><strong>Against:</strong> <a href="consultant-profile.php?id=$consultantId">$consultantName</a></p>
+                                    <div class="complaint-description">
+                                        <h6 class="text-uppercase">description</h6>
+                                        <p class="p-4">"$description"</p>
+                                    </div>
+                                    <div class="complaint-feedback">
+                                        <h6 class="text-uppercase">Feedback</h6>
+                                        <p class="p-4">"$feedback"</p>
+                                    </div>
+                                    <div class="update-complaint-status">
+                                        <form action="" method="post">
+                                            <input type="hidden" name="complaint-id" value="$id">
+                                            <div class="form-group">
+                                                <label for="">Update Complaint Status</label>
+                                                <select class="form-control" name="complaint-status">
+                                                    <option value="resolved">Resolved</option>
+                                                    <option value="inprocess">In Process</option>
+                                                    <option value="pending">Pending</option>
+                                                    <option value="completed">Completed</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Give Your Feedback</label>
+                                                <textarea name="complaint-feedback" class="form-control"></textarea>
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" name="updateStatus" value="updateStatus" class="btn btn-dark">Update Status</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+            <tr>
+        HTML;
+        echo $td;
+}
+
+function bookingList($data) {
+    array_map("bookingTd", $data);
+}
+
+function complaintList($data) {
+    array_map("complaintTd", $data);
+}
+
+if(isset($_POST['updateStatus'])) {
+    $admin->updateComplaintStatus($_POST['complaint-id'], $_POST['complaint-status']);
+    $admin->updateComplaintFeedback($_POST['complaint-id'], $_POST['complaint-feedback']);
+    header("Refresh:1");
+}
